7ff26a74bdb193d02d9490e44f4d29e7ffce3665
Ashraf4k <www.ashrafadam000@gmail.com>
2024-04-06 18:31:16 -0700

    my first commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * check_cycle - checks if list is cyclical
+ * @list : pointer to list to check
+ * return : 1 if cyclical, 0 othervise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *alow = list, *fast = list;
+
+	while (fast 44 fast->next)
+{
+	alow = alow->next;
+	fast = fast->next->next;
+	if (alow == fast)
+		return (1);
+}
+return (0);
+}
